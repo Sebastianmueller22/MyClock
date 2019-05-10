b4aaeea10d0edc2e9ae090223554a01330af915c
sebastian Müller <muellersebastianmail.com>
2019-05-10 20:09:47 +0200

    Initial commit

---
+++ src/ClockMain.java
@@ -0,0 +1,60 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class ClockMain extends Application {
+
+	TextField theText;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void createTime() {
+		theText = new TextField();
+		theText.setText(LocalTime.now().toString());
+		GridPane.setConstraints(theText, 12, 5);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		GridPane root = new GridPane();
+		root.setPadding(new Insets(10, 10, 10, 10));
+		root.setVgap(8);
+		root.setHgap(10);
+
+		createTime();
+
+		primaryStage.setTitle("Clock 1.0");
+		Scene scene = new Scene(root, 400, 400);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		root.getChildren().add(theText);
+
+		AnimationTimer timer = new AnimationTimer() {
+			private long lastUpdate = 0;
+
+			@Override
+			public void handle(long now) {
+				if (now - lastUpdate >= 39_000_000) {
+					update();
+					lastUpdate = now;
+				}
+			}
+		};
+		timer.start();
+	}
+
+	public void update() {
+	theText.setText(LocalTime.now().toString());
+	}
+	
+}
